67452f605efaeccc3f7c922b6129609ed084854e
suleymangunes <suleymangunesmail@gmail.com>
2025-05-01 14:35:43 +0300

    string

---
+++ day_2/ep_4/string.py
@@ -0,0 +1,17 @@
+isim = "kodla"
+
+aciklama = """Bu birden fazla
+satirdan olusan
+bir yazidir.
+"""
+
+yazi = "kodla"
+
+print(yazi[1:3])
+print(yazi[:3])
+print(yazi[1:])
+print(yazi[::-1])
+print(yazi[4:1:-1])
+
+yeni = "a" + yazi[1:]
+print(yeni)
