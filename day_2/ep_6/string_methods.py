48e2a0ef122bbc0ad01273867812308de6e8129a
suleymangunes <suleymangunesmail@gmail.com>
2025-05-03 22:48:16 +0300

    string methods

---
+++ day_2/ep_6/string_methods.py
@@ -0,0 +1,21 @@
+print("kodla".upper())
+
+print("KODLA".lower())
+
+print("           kodla           ".strip())
+
+print("           kodla           ".lstrip())
+
+print("           kodla           ".rstrip())
+
+print("merhaba kodla".replace("kodla", "dünya"))
+
+print("kodla".find("d"))
+
+print(len("kodla"))
+
+print("kodla".capitalize())
+
+print("merhaba kodla".title())
+
+print("Kodla".swapcase())
