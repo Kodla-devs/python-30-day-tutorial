620a06192806fd821e48db1fa939fd4c53d852c2
suleymangunes <suleymangunesmail@gmail.com>
2025-05-06 12:31:53 +0300

    integer

---
+++ day_3/ep_1/integer.py
@@ -0,0 +1,19 @@
+yas = 35
+
+print(type(yas))
+
+negatif = -10
+sifir = 0
+
+print(type(negatif))
+print(type(sifir))
+
+sayi2 = "35"
+print(type(sayi2))
+
+uzun_sayi = 13646986567987956564676535676
+print(uzun_sayi)
+
+int_sayi = int(45)
+print(int_sayi)
+print(type(int_sayi))
