2ca8b677a91a160e3552eed2ff48f3ba45e5f9c4
suleymangunes <suleymangunesmail@gmail.com>
2025-05-11 00:04:09 +0300

    math

---
+++ day_3/ep_3/math.py
@@ -0,0 +1,15 @@
+import math
+
+print(math.sqrt(25))
+
+print(math.pow(2, 3))
+
+print(math.ceil(4.5))
+print(math.floor(4.5))
+
+print(math.sin(math.radians(30)))
+print(math.cos(math.radians(30)))
+print(math.tan(math.radians(30)))
+
+print(math.pi)
+print(math.e)
