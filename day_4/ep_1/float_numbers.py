2b9d0279d3b825a043e697cc09e8a6b37f247e19
suleymangunes <suleymangunesmail@gmail.com>
2025-05-19 11:04:19 +0300

    float

---
+++ day_4/ep_1/float_numbers.py
@@ -0,0 +1,23 @@
+pi = 3.14
+
+print(type(pi))
+
+negatif_ondalikli = -2.5
+
+x = float(5)
+
+print(x)
+print(type(x))
+
+
+print(0.1 + 0.5)
+
+print(0.1 - 0.5)
+
+print(0.1 * 0.5)
+
+print(0.1 / 0.5)
+
+print(1 + 2.0)
+
+print(0.1 + 0.2)
