636e078290fc2aacb7619c705bfed64112ffb5a6
suleymangunes <suleymangunesmail@gmail.com>
2025-05-28 16:54:01 +0300

    comparison operators

---
+++ day_4/ep_5/comparison_operators.py
@@ -0,0 +1,21 @@
+print(4 == 4)
+
+print(4 != 5)
+
+print(3 < 5)
+
+print(10 > 100)
+
+print(7 <= 7)
+
+print(8 >= 9)
+
+print(5 == 5.0)
+
+print("kodla" == "kodla")
+
+print("Kodla" == "kodla")
+
+x = 5
+
+print(3 < x < 10)
