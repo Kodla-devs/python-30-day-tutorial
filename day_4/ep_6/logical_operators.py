52b8065cfe2e4d855fc35f7673c048204156194a
suleymangunes <suleymangunesmail@gmail.com>
2025-05-31 12:32:07 +0300

    logical operators

---
+++ day_4/ep_6/logical_operators.py
@@ -0,0 +1,16 @@
+giris_yapti_mi = False
+
+print(giris_yapti_mi)
+print(not giris_yapti_mi)
+
+print(not 0)
+
+print(not 1)
+
+print(not "")
+
+print(not "kodla")
+
+name = "" or "kodla"
+
+print(name)
