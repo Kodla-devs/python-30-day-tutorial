b34874978862d20a872ead5e696b95880cad0538
suleymangunes <suleymangunesmail@gmail.com>
2025-06-16 13:25:39 +0300

    type casting

---
+++ day_5/ep_2/type_casting.py
@@ -0,0 +1,15 @@
+yas = int(input("yasinizi girin: "))
+
+print(yas > 18)
+
+kilo = float(input("kilonuzu girin: "))
+
+print(kilo > 80)
+
+giris = bool(input("giris yapildi mi?: "))
+
+print(giris)
+
+print(type(yas))
+print(type(kilo))
+print(type(giris))
