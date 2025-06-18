524bfff9957951a26d7725650919675baa280702
suleymangunes <suleymangunesmail@gmail.com>
2025-06-18 15:05:55 +0300

    basic calculator

---
+++ day_5/ep_3/basic_calculator.py
@@ -0,0 +1,22 @@
+s1 = int(input("birinci sayi: "))
+s2 = int(input("ikinci sayi: "))
+
+print("toplama: ", s1 + s2)
+print("cikarma: ", s1 - s2)
+print("carpma: ", s1 * s2)
+print("bölme: ", s1 / s2)
+
+s3 = float(input("ucuncu sayı: "))
+s4 = float(input("dorduncu sayi: "))
+
+print("toplama: ", s3 + s4)
+print("cikarma: ", s3 - s4)
+print("carpma: ", s3 * s4)
+print("bölme: ", s3 / s4)
+
+isim = input("isminizi girin: ")
+yas = int(input("yasinizi girin: "))
+
+print("isim: " + isim + " yas: " + str(yas)) 
+
+print(f"isim: {isim} yas: {yas}")
