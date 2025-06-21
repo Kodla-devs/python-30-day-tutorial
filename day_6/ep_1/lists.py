a4c0f033233e2d00f0704f60859ed5eb123ba2f7
suleymangunes <suleymangunesmail@gmail.com>
2025-06-21 15:35:02 +0300

    lists

---
+++ day_6/ep_1/lists.py
@@ -0,0 +1,16 @@
+liste = []
+
+sayi_listesi = [1, 2, 3, 4, 5]
+
+kelimeler = ["kodla", "kanalına", "hoşgeldin"]
+
+dogruluklar = [True, False, True]
+
+karisik = [42, "kodla", True, 3.1, ["yeni", "liste", 2], 3, 4, 6, 78, "merhaba", "sonsuz", "liste", True]
+
+print(karisik)
+print(type(karisik))
+
+kodla = list("kodla")
+
+print(kodla)
