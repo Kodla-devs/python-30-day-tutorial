f9aba19cc5fbcf6d360e24902f9cb0d6d13791ce
suleymangunes <suleymangunesmail@gmail.com>
2025-06-23 00:05:59 +0300

    list indexing

---
+++ day_6/ep_2/list_indexing.py
@@ -0,0 +1,15 @@
+egzersizler = ["squat", "deadlift", "dips", "barfiks"]
+
+print(egzersizler[0])
+
+print(egzersizler[1])
+
+print(egzersizler[2])
+
+print(egzersizler[3])
+
+egzersizler[2] = "plank"
+
+print(egzersizler)
+
+print(egzersizler[5])
