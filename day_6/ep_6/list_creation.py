0720c856c4a2e3420a6819f74818767668be4111
suleymangunes <suleymangunesmail@gmail.com>
2025-07-05 11:10:30 +0300

    list creation

---
+++ day_6/ep_6/list_creation.py
@@ -0,0 +1,20 @@
+liste = list(range(5))
+
+print(liste)
+
+liste = list(range(2, 6))
+
+liste = list(range(0, 10, 2))
+
+liste = list(range(5, 0, -1))
+
+tekrarli_liste = [0] *5
+
+tekrarli_liste = [[0] * 3] * 3
+
+diller = "python,java,dart,swift"
+
+split_liste = diller.split(",")
+
+string_liste = list("kodla")
+
